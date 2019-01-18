0ab1032d15587f2b4a436e6815c050ce5908ab5f
Tim Poot <tpoot96@gmail.com>
2019-01-18 18:06:13 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    	<meta charset="utf-8">
+    	<link rel="stylesheet" type="text/css" href="./style/index.css">
+    	<title> Een Leuke Prezi </title>
+    </head>
+
+    <body>
+        <div id="begin" class="text"> Knorrenkechs </div>
+    </body>
+</html>
+
